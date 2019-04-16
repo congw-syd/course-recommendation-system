b036220a3ee5f42ff15f115f122d978f395541b3
Britta <41138162+congw-syd@users.noreply.github.com>
2019-04-17 09:04:07 +1000

    Add files via upload

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,33 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import {courses} from '../data/courses';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll'
+import Scroll from '../components/Scroll';
+import Navigation from '../components/Navigation';
+import Signin from '../components/Signin';
+import Particles from 'react-particles-js';
+import Register from '../components/Register';
+import PersonInfo from '../components/PersonInfo';
 import './App.css';
+
+const particlesOptions = {
+	particles: {
+    	number:{
+    		value: 60,
+    		density:{
+    			enable: true,
+    			value_area: 700
+    		}
+    	}	
+    }
+}
 
 class App extends Component {
 	constructor(){
 		super()
 		this.state={
 			courses: [],
-			searchfiled: ''
+			searchfiled: '',
+			route: 'signin'
 		}
 	}
 
@@ -22,8 +40,12 @@ class App extends Component {
 		this.setState({searchfiled: event.target.value })
 	}
 
+	onRouteChange = (route) =>{
+		this.setState ({route: route});
+	}
+
 	render(){
-		const { courses, searchfiled } = this.state;
+		const { courses, searchfiled, route } = this.state;
 		const filterCourse = courses.filter(item =>{
 			return item.name.toLowerCase().includes(searchfiled.toLowerCase());
 		})
@@ -32,11 +54,26 @@ class App extends Component {
 			<h1>Loading</h1> :
 			(
 				<div className = 'tc'>
+					<Particles className ='particles' params ={particlesOptions} />
 					<h1 className = 'f1'>Course Recommender</h1>
-					<SearchBox searchChange = {this.onSearchChange}/>
-					<Scroll>
-						<CardList courses={filterCourse}/>
-					</Scroll>
+					{ route === 'home' ?
+						<div>
+							<div>
+								<SearchBox searchChange = {this.onSearchChange}/>
+								<Navigation onRouteChange={this.onRouteChange}/>
+							</div>
+							<Scroll>
+								<CardList courses={filterCourse}/>
+							</Scroll>
+						</div>
+					: (
+						route === 'signin' ?
+						<Signin onRouteChange={this.onRouteChange}/> 
+						: route === 'personinfo' ?
+							<PersonInfo onRouteChange={this.onRouteChange}/>
+							: <Register onRouteChange={this.onRouteChange}/>
+					)		
+				}
 				</div>
 			);
 	}
