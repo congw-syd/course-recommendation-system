2aa39f81c06bb067d912fb8a649f4772611687ed
Britta <41138162+congw-syd@users.noreply.github.com>
2019-04-18 17:40:54 +1000

    Add files via upload

--- back-end/server.js
+++ back-end/server.js
@@ -1,25 +1,26 @@
+/*
+/signin -- > POST = success/fail
+/register --> POST =user
+/profile/ : userId --> GET = user
+*/
+
 const express = require ('express');
 const bodyParser = require('body-parser');
 const cors = require ('cors');
+const knex = require ('knex');
+
+const db = knex ({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'apple',
+    password : '',
+    database : 'courserecom'
+  }
+});
 
 const app = express();
 
-const database ={
-	users: [
-	{
-		id:'123',
-		name: 'john',
-		email: 'john@gmail.com',
-		password: 'cookies',
-	},
-	{
-		id:'124',
-		name: 'sally',
-		email: 'sally@gmail.com',
-		password: 'bananas',
-	}
-	]
-}
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -29,36 +30,49 @@ app.get('/', (req, res) =>{
 })
 
 app.post('/signin', (req, res) =>{
-	if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-		res.json(database.user[0]);
-	}else{
-		res.status(400).json('error logging in');
-	}
+	db.select('email','password').from('users')
+	  .where('email', '=', req.body.email)
+	  .then(data => {
+	  	if(req.body.password === data[0].password){
+	  		return db.select('*').from('users')
+		  		  	 .where('email','=',req.body.email)
+		  		     .then(user =>{
+		  		  	    res.json(user[0])
+		  		     })
+		  		     .catch(err => res.status(400).json('unable to get user'))
+		}else{
+			res.status(400).json('wrong credentials')
+		}	
+	  })
+	  .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) =>{
 	const {email, name, password} = req.body;
-	database.users.push({
-		id: '125',
+	db('users')
+		.returning('*')
+		.insert({
 		name: name,
 		email: email,
 		password: password
-	});
-	res.json(database.users[database.users.length-1]);
+		})
+		.then(user => {
+		res.json(user[0]);
+		})
+		.catch(err => res.status(400).json('unable to register'))
 })
 
-app.get('/profile/:id', (req,res)=>{
-	const {id} = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if(user.id === id){
-			found = true;
-			return res.json(user);
-		}
-	})
-	if(!found){
-		res.status(400).json('not found');
-	}
+app.get('/profile/:userid', (req,res)=>{
+	const {userid} = req.params;
+	db.select('*').from('users').where({userid})
+		.then(user => {
+			if(user.length){
+				res.json(user[0])
+			}else{
+				res.status(400).json('Not Found')
+			}
+		})
+		.catch( err => res.status(400).json('error getting user'))
 })
 
 
@@ -67,8 +81,3 @@ app.listen(3000, () => {
 	console.log('app is running on port 3000');
 })
 
-/*
-/signin -- > POST = success/fail
-/register --> POST =user
-/profile/ : userId --> GET = user
-*/
